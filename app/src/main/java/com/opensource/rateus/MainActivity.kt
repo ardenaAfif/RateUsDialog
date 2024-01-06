f18d93eaa4d9484cbad64ff4ae14eb90d7a6140c
aafif <aafifpratama@gmail.com>
2024-01-06 21:14:26 +0700

    Add Rate Us Dialog

--- app/src/main/java/com/opensource/rateus/MainActivity.kt
+++ app/src/main/java/com/opensource/rateus/MainActivity.kt
@@ -1,15 +1,32 @@
 package com.opensource.rateus
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.opensource.rateus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.btnRateUs.setOnClickListener {
+            showRatingDialog()
+        }
+
+    }
+
+    private fun showRatingDialog() {
+        val rateUsDialog = RateUsDialog(this)
+        rateUsDialog.window?.setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.transparant)))
+        rateUsDialog.setCancelable(false)
+        rateUsDialog.show()
     }
 }
---
+++ app/src/main/java/com/opensource/rateus/RateUsDialog.kt
@@ -0,0 +1,62 @@
+package com.opensource.rateus
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.animation.Animation
+import android.view.animation.ScaleAnimation
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDialog
+import com.opensource.rateus.databinding.RateUsDialogBinding
+
+class RateUsDialog(context: Context) : AppCompatDialog(context) {
+
+    // Declare variables
+    private lateinit var binding: RateUsDialogBinding
+
+    init {
+        // Initialize views using ViewBinding
+        binding = RateUsDialogBinding.inflate(LayoutInflater.from(context))
+        setContentView(binding.root)
+
+        binding.apply {
+            btnClose.setOnClickListener {
+                dismiss()
+            }
+
+            btnSubmit.setOnClickListener {
+                showFeedbackToast()
+            }
+        }
+
+        // Play animation
+        playAnim(binding.ivEmoRating)
+    }
+
+    private fun showFeedbackToast() {
+        val rating = binding.ratingBar.rating
+        val message = binding.etMessage.text
+
+        val feedbackMessage = "Rating $rating\n Message: $message"
+        Toast.makeText(context, feedbackMessage, Toast.LENGTH_SHORT).show()
+
+        dismiss()
+    }
+
+    private fun playAnim(ratingEmo: ImageView) {
+        val scaleAnim = ScaleAnimation(
+            0f,
+            1f,
+            0f,
+            1f,
+            Animation.RELATIVE_TO_SELF,
+            0.5f,
+            Animation.RELATIVE_TO_SELF,
+            0.5f
+        )
+
+        scaleAnim.fillAfter = true
+        scaleAnim.duration = 200
+        ratingEmo.startAnimation(scaleAnim)
+    }
+}
